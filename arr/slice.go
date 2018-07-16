c86356509fab703f3544cc8939013faad7764496
inhere <in.798@qq.com>
2018-07-16 10:17:59 +0800

    update

---
+++ arr/slice.go
@@ -0,0 +1,12 @@
+package arr
+
+// Reverse string slice [site user info 0] -> [0 info user site]
+func Reverse(ss []string) {
+	ln := len(ss)
+
+	for i := 0; i < int(ln/2); i++ {
+		li := ln - i - 1
+		// fmt.Println(i, "<=>", li)
+		ss[i], ss[li] = ss[li], ss[i]
+	}
+}
