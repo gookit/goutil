3a3b1184b6cec724d21a85b3a205de7c3ced1c5a
inhere <in.798@qq.com>
2018-12-07 15:56:26 +0800

    add some map functions.

--- maps/map.go
+++ maps/map.go
@@ -1,1 +1,28 @@
 package maps
+
+import "strings"
+
+// MergeStringMap simple merge two string map
+func MergeStringMap(src, dst map[string]string, ignoreCase bool) map[string]string {
+	for k, v := range src {
+		if ignoreCase {
+			k = strings.ToLower(k)
+		}
+
+		dst[k] = v
+	}
+
+	return dst
+}
+
+// KeyToLower convert keys to lower case.
+func KeyToLower(src map[string]string) map[string]string {
+	newMp := make(map[string]string)
+
+	for k, v := range src {
+		k = strings.ToLower(k)
+		newMp[k] = v
+	}
+
+	return newMp
+}
