5f59448aa33d364ed718a460b9da164dffadfc8a
inhere <in.798@qq.com>
2019-03-14 02:42:31 +0800

    add process check util form docker codes

---
+++ sysutil/process/process_windows.go
@@ -0,0 +1,26 @@
+package process
+
+import (
+	"golang.org/x/sys/windows"
+)
+
+const (
+	processQueryLimitedInformation = 0x1000
+
+	stillActive = 259
+)
+
+// Exists check process running by given pid
+func Exists(pid int) bool {
+	h, err := windows.OpenProcess(processQueryLimitedInformation, false, uint32(pid))
+	if err != nil {
+		return false
+	}
+	var c uint32
+	err = windows.GetExitCodeProcess(h, &c)
+	windows.Close(h)
+	if err != nil {
+		return c == stillActive
+	}
+	return true
+}
