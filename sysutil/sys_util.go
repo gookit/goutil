a8af717c3867fcedd297db26a936c30c67edb3e2
inhere <in.798@qq.com>
2019-03-17 16:05:54 +0800

    update sys util

--- sysutil/sys_util.go
+++ sysutil/sys_util.go
@@ -1,1 +1,13 @@
 package sysutil
+
+import "syscall"
+
+// Kill process by pid
+func Kill(pid int, signal syscall.Signal) error {
+	return syscall.Kill(pid, signal)
+}
+
+// ProcessExists check process exists by pid
+func ProcessExists(pid int) bool {
+	return nil == syscall.Kill(pid, 0)
+}
