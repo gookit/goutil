36f16fd800763a2661d820b3898dcedcbd4f9c1f
inhere <in.798@qq.com>
2020-01-02 20:17:37 +0800

    add tool struct

---
+++ value.go
@@ -0,0 +1,71 @@
+package goutil
+
+import (
+	"fmt"
+
+	"github.com/gookit/goutil/mathutil"
+)
+
+// Value data store
+type Value struct {
+	// V value
+	V interface{}
+}
+
+// Reset value
+func (v *Value) Reset()  {
+	v.V = nil
+}
+
+// Val get
+func (v Value) Val() interface{} {
+	return v.V
+}
+
+// Int value
+func (v Value) Int() int {
+	if v.V == nil {
+		return 0
+	}
+
+	return mathutil.MustInt(v.V)
+}
+
+// Int64 value
+func (v Value) Int64() int64 {
+	if v.V == nil {
+		return 0
+	}
+
+	return mathutil.MustInt64(v.V)
+}
+
+// String value
+func (v Value) String() string {
+	if v.V == nil {
+		return ""
+	}
+
+	if str, ok := v.V.(string); ok {
+		return str
+	}
+
+	return fmt.Sprintf("%v", v.V)
+}
+
+// Strings value
+func (v Value) Strings() (ss []string) {
+	if v.V == nil {
+		return
+	}
+
+	if ss, ok := v.V.([]string); ok {
+		return ss
+	}
+	return
+}
+
+// IsEmpty value
+func (v Value) IsEmpty() bool {
+	return v.V == nil
+}
---
+++ value_test.go
@@ -0,0 +1,26 @@
+package goutil_test
+
+import (
+	"testing"
+
+	"github.com/gookit/goutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValue_Val(t *testing.T) {
+	v := goutil.Value{V: 23}
+
+	assert.Equal(t, 23, v.Val())
+	assert.Equal(t, 23, v.Int())
+	assert.Equal(t, int64(23), v.Int64())
+	assert.Equal(t, "23", v.String())
+	assert.False(t, v.IsEmpty())
+
+	v.V = []string{"a", "b"}
+	assert.Equal(t, []string{"a", "b"}, v.Val())
+	assert.Equal(t, []string{"a", "b"}, v.Strings())
+
+	v.Reset()
+	assert.Nil(t, v.V)
+	assert.True(t, v.IsEmpty())
+}
