6d4a1f964c82f78573e280cf9d8d7ca09cb5ebff
inhere <in.798@qq.com>
2020-09-05 10:58:41 +0800

    update some http content type const

---
+++ netutil/httpctype/mime_type.go
@@ -0,0 +1,19 @@
+package httpctype
+
+// Content-Type MIME of the most common data formats.
+const (
+	MIMEHTML  = "text/html"
+	MIMEText  = "text/plain" // equals MIMEPlain
+	MIMEPlain = "text/plain"
+	MIMEJSON  = "application/json"
+	MIMEXML   = "application/xml"
+	MIMEXML2  = "text/xml"
+	MIMEYAML  = "application/x-yaml"
+
+	MIMEPOSTForm      = "application/x-www-form-urlencoded"
+	MIMEMultiDataForm = "multipart/form-data"
+
+	MIMEPROTOBUF = "application/x-protobuf"
+	MIMEMSGPACK  = "application/x-msgpack"
+	MIMEMSGPACK2 = "application/msgpack"
+)
