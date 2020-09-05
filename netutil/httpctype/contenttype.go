5b046e921b16885888a38015cd6ce40a6b087737
inhere <in.798@qq.com>
2020-09-05 10:40:14 +0800

    add some const for http content type

---
+++ netutil/httpctype/contenttype.go
@@ -0,0 +1,16 @@
+// Package httpctype list some common http content-type
+package httpctype
+
+var Key = "Content-Type"
+var (
+	CSS   = "text/css; charset=UTF-8"
+	HTML  = "text/html; charset=UTF-8"
+	Text  = "text/plain; charset=UTF-8"
+	XML   = "application/xml; charset=UTF-8"
+
+	JSON  = "application/json; charset=utf-8"
+	JSONP = "application/javascript; charset=UTF-8" // equals to JS
+
+	JS1 = "text/javascript; charset=UTF-8"
+	JS = "application/javascript; charset=UTF-8"
+)
