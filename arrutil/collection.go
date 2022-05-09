ce8323f76820a6cf8ee9806bf2d6d595c8faa7e5
andy <mykge@outlook.com>
2022-05-09 14:12:29 +0800

    fix bugs; add unit tests for collection

--- arrutil/collection.go
+++ arrutil/collection.go
@@ -5,29 +5,42 @@ import (
 	"reflect"
 )
 
+const ErrElementNotFound = "element not found"
+
+type Comparer func(a, b interface{}) int
+type Predicate func(a interface{}) bool
+
 var (
-	// 对比字符串
-	StringEqualCompareFunc = func(a, b interface{}) int {
-		if reflect.TypeOf(a) != reflect.TypeOf(b) {
+	StringEqualsComparer Comparer = func(a, b interface{}) int {
+		typeOfA := reflect.TypeOf(a)
+		if typeOfA.Kind() == reflect.Ptr {
+			typeOfA = typeOfA.Elem()
+		}
+
+		typeOfB := reflect.TypeOf(b)
+		if typeOfB.Kind() == reflect.Ptr {
+			typeOfB = typeOfB.Elem()
+		}
+
+		if typeOfA != typeOfB {
 			return -1
 		}
+
 		strA := ""
 		strB := ""
 
-		at := reflect.TypeOf(a)
-		if at.Kind() == reflect.String {
-			strA = a.(string)
-		} else if at.Kind() == reflect.Ptr && at.Elem().Kind() == reflect.String {
-			strA = *a.(*string)
+		if val, ok := a.(string); ok {
+			strA = val
+		} else if val, ok := a.(*string); ok {
+			strA = *val
 		} else {
 			return -1
 		}
 
-		bt := reflect.TypeOf(b)
-		if bt.Kind() == reflect.String {
-			strB = b.(string)
-		} else if bt.Kind() == reflect.Ptr && bt.Elem().Kind() == reflect.String {
-			strB = *b.(*string)
+		if val, ok := b.(string); ok {
+			strB = val
+		} else if val, ok := b.(*string); ok {
+			strB = *val
 		} else {
 			return -1
 		}
@@ -35,19 +48,17 @@ var (
 		if strA == strB {
 			return 0
 		}
-		return 1
+		return -1
 	}
 
-	// 对比指针地址，如果相同则返回0，否则返回1
-	EqualCompareFunc = func(a, b interface{}) int {
+	ReferenceEqualsComparer Comparer = func(a, b interface{}) int {
 		if a == b {
 			return 0
 		}
-		return 1
+		return -1
 	}
 
-	// 对比对象的类型，如果相同则返回0，否则返回1
-	ElemTypeEqualCompareFunc = func(a, b interface{}) int {
+	ElemTypeEqualsComparer Comparer = func(a, b interface{}) int {
 		at := reflect.TypeOf(a)
 		bt := reflect.TypeOf(b)
 		if at.Kind() == reflect.Ptr {
@@ -61,248 +72,213 @@ var (
 		if at == bt {
 			return 0
 		}
-		return 1
-
+		return -1
 	}
 )
 
-// 检查数组中是否包含指定的元素
-// data: 数组
-// item: 指定的元素
-// comparer: 比较器
-// return: 包含返回true, 否则返回false
-func ContainsItem(data interface{}, item interface{}, comparer func(a interface{}, b interface{}) int) bool {
+func TwowaySearch(data interface{}, item interface{}, fn Comparer) (int, error) {
+	if data == nil {
+		return -1, errors.New("collections.TwowaySearch: data is nil")
+	}
+	if fn == nil {
+		return -1, errors.New("collections.TwowaySearch: fn is nil")
+	}
+
 	dataType := reflect.TypeOf(data)
-	if dataType.Kind() != reflect.Slice && dataType.Kind() != reflect.Array {
-		panic("data must be a slice or array")
+	if dataType.Kind() != reflect.Slice {
+		return -1, errors.New("collections.TwowaySearch: data is not a slice")
 	}
-	s := reflect.ValueOf(data)
-	for i := 0; i < s.Len(); i++ {
-		if comparer(s.Index(i).Interface(), item) == 0 {
-			return true
+
+	dataVal := reflect.ValueOf(data)
+	if dataVal.Len() == 0 {
+		return -1, errors.New("collections.TwowaySearch: data is empty")
+	}
+	itemType := dataType.Elem()
+	if itemType.Kind() == reflect.Ptr {
+		itemType = itemType.Elem()
+	}
+
+	if itemType != dataVal.Index(0).Type() {
+		return -1, errors.New("collections.TwowaySearch: item type is not the same as data type")
+	}
+
+	forward := 0
+	backward := dataVal.Len() - 1
+
+	for forward <= backward {
+		forwardVal := dataVal.Index(forward).Interface()
+		if fn(forwardVal, item) == 0 {
+			return forward, nil
 		}
+
+		backwardVal := dataVal.Index(backward).Interface()
+		if fn(backwardVal, item) == 0 {
+			return backward, nil
+		}
+
+		forward++
+		backward--
 	}
-	return false
+
+	return -1, errors.New(ErrElementNotFound)
 }
 
-// 在两个数组之间取差集
-// a: 数组a
-// b: 数组b
-// comparer: 比较器
-// return: 差集
-func Excepts(source interface{}, data interface{}, comparer func(a interface{}, b interface{}) int) interface{} {
-	aType := reflect.TypeOf(source)
-	if aType.Kind() != reflect.Slice && aType.Kind() != reflect.Array {
-		panic("collections.Excepts: source must be a slice or array")
-	}
-	var itemType reflect.Type
-	sourceVal := reflect.ValueOf(source)
-	if sourceVal.Len() == 0 {
-		return data
-	} else {
-		itemType = sourceVal.Index(0).Type()
-	}
-
-	bType := reflect.TypeOf(data)
-	if bType.Kind() != reflect.Slice && bType.Kind() != reflect.Array {
-		panic("collections.Excepts: data must be a slice or array")
-	}
-
-	bVal := reflect.ValueOf(data)
-	if bVal.Len() == 0 {
-		return source
-	}
-
-	result := reflect.New(reflect.ArrayOf(0, itemType)).Elem()
-	for i := 0; i < sourceVal.Len(); i++ {
-		for j := 0; j < bVal.Len(); j++ {
-			s := sourceVal.Index(i).Interface()
-			d := bVal.Index(j).Interface()
-			if comparer(s, d) != 0 {
-				result = reflect.Append(result, reflect.ValueOf(s))
-			}
-		}
-	}
-
-	return result
+func MakeEmptySlice(itemType reflect.Type) interface{} {
+	ret := reflect.MakeSlice(reflect.SliceOf(itemType), 0, 0).Interface()
+	return ret
 }
 
-// 在两个数组之间取交集
-// a: 数组a
-// b: 数组b
-// comparer: 比较器
-// return: 交集
-func Intersects(source interface{}, data interface{}, comparer func(a interface{}, b interface{}) int) interface{} {
-	aType := reflect.TypeOf(source)
-	if aType.Kind() != reflect.Slice && aType.Kind() != reflect.Array {
-		panic("collections.Excepts: source must be a slice or array")
+func CloneSlice(data interface{}) interface{} {
+	typeOfData := reflect.TypeOf(data)
+	if typeOfData.Kind() != reflect.Slice {
+		panic("collections.CloneSlice: data must be a slice")
 	}
-	var itemType reflect.Type
-	sourceVal := reflect.ValueOf(source)
-	if sourceVal.Len() == 0 {
-		return data
-	} else {
-		itemType = sourceVal.Index(0).Type()
-	}
-
-	bType := reflect.TypeOf(data)
-	if bType.Kind() != reflect.Slice && bType.Kind() != reflect.Array {
-		panic("collections.Excepts: data must be a slice or array")
-	}
-
-	bVal := reflect.ValueOf(data)
-	if bVal.Len() == 0 {
-		return source
-	}
-
-	result := reflect.New(reflect.ArrayOf(0, itemType)).Elem()
-	for i := 0; i < sourceVal.Len(); i++ {
-		for j := 0; j < bVal.Len(); j++ {
-			s := sourceVal.Index(i).Interface()
-			d := bVal.Index(j).Interface()
-			if comparer(s, d) == 0 {
-				result = reflect.Append(result, reflect.ValueOf(s))
-			}
-		}
-	}
-
-	return result
+	return reflect.AppendSlice(reflect.New(reflect.SliceOf(typeOfData.Elem())).Elem(), reflect.ValueOf(data)).Interface()
 }
 
-// 在两个数组之间取并集
-// a: 数组a
-// b: 数组b
-// comparer: 比较器
-func Union(source interface{}, data interface{}, comparer func(a interface{}, b interface{}) int) interface{} {
-	aType := reflect.TypeOf(source)
-	if aType.Kind() != reflect.Slice && aType.Kind() != reflect.Array {
-		panic("collections.Excepts: source must be a slice or array")
+func Excepts(first interface{}, second interface{}, fn Comparer) interface{} {
+	typeOfFirst := reflect.TypeOf(first)
+	if typeOfFirst.Kind() != reflect.Slice {
+		panic("collections.Excepts: first must be a slice")
 	}
-	var itemType reflect.Type
-	sourceVal := reflect.ValueOf(source)
-	if sourceVal.Len() == 0 {
-		return data
-	} else {
-		itemType = sourceVal.Index(0).Type()
+	valOfFirst := reflect.ValueOf(first)
+	if valOfFirst.Len() == 0 {
+		return MakeEmptySlice(typeOfFirst.Elem())
 	}
 
-	bType := reflect.TypeOf(data)
-	if bType.Kind() != reflect.Slice && bType.Kind() != reflect.Array {
-		panic("collections.Excepts: data must be a slice or array")
+	typeOfSecond := reflect.TypeOf(second)
+	if typeOfSecond.Kind() != reflect.Slice {
+		panic("collections.Excepts: second must be a slice")
 	}
 
-	bVal := reflect.ValueOf(data)
-	if bVal.Len() == 0 {
-		return source
+	valOfSecond := reflect.ValueOf(second)
+	if valOfSecond.Len() == 0 {
+		return CloneSlice(first)
 	}
 
-	result := reflect.New(reflect.ArrayOf(0, itemType)).Elem()
-	for i := 0; i < sourceVal.Len(); i++ {
-		for j := 0; j < result.Len(); j++ {
-			s := sourceVal.Index(i).Interface()
-			d := result.Index(j).Interface()
-			if comparer(s, d) != 0 {
-				result = reflect.Append(result, reflect.ValueOf(s))
-			}
+	result := reflect.New(reflect.SliceOf(typeOfFirst.Elem())).Elem()
+	for i := 0; i < valOfFirst.Len(); i++ {
+		s := valOfFirst.Index(i).Interface()
+		if i, _ := TwowaySearch(second, s, fn); i < 0 {
+			result = reflect.Append(result, reflect.ValueOf(s))
 		}
 	}
-	for i := 0; i < bVal.Len(); i++ {
-		for j := 0; j < result.Len(); j++ {
-			s := bVal.Index(i).Interface()
-			d := result.Index(j).Interface()
-			if comparer(s, d) != 0 {
-				result = reflect.Append(result, reflect.ValueOf(s))
-			}
-		}
-	}
-	return result
+
+	return result.Interface()
 }
 
-var ErrElementNotFound = errors.New("element not found")
+func Intersects(first interface{}, second interface{}, fn Comparer) interface{} {
+	typeOfFirst := reflect.TypeOf(first)
+	if typeOfFirst.Kind() != reflect.Slice {
+		panic("collections.Intersects: first must be a slice")
+	}
+	valOfFirst := reflect.ValueOf(first)
+	if valOfFirst.Len() == 0 {
+		return MakeEmptySlice(typeOfFirst.Elem())
+	}
 
-// 在数组中查找符合条件的元素, 如果没有找到, 则返回 ErrElementNotFound
-// source: 数组
-// predicate: 条件
-// return: 符合条件的元素
-func Find(source interface{}, predicate func(a interface{}) bool) (interface{}, error) {
+	typeOfSecond := reflect.TypeOf(second)
+	if typeOfSecond.Kind() != reflect.Slice {
+		panic("collections.Intersects: second must be a slice")
+	}
+
+	valOfSecond := reflect.ValueOf(second)
+	if valOfSecond.Len() == 0 {
+		return MakeEmptySlice(typeOfFirst.Elem())
+	}
+
+	result := reflect.New(reflect.SliceOf(typeOfFirst.Elem())).Elem()
+	for i := 0; i < valOfFirst.Len(); i++ {
+		s := valOfFirst.Index(i).Interface()
+		if i, _ := TwowaySearch(second, s, fn); i >= 0 {
+			result = reflect.Append(result, reflect.ValueOf(s))
+		}
+	}
+
+	return result.Interface()
+}
+
+func Union(first interface{}, second interface{}, fn Comparer) interface{} {
+	excepts := Excepts(second, first, fn)
+
+	typeOfFirst := reflect.TypeOf(first)
+	if typeOfFirst.Kind() != reflect.Slice {
+		panic("collections.Intersects: first must be a slice")
+	}
+	valOfFirst := reflect.ValueOf(first)
+	if valOfFirst.Len() == 0 {
+		return CloneSlice(second)
+	}
+
+	result := reflect.AppendSlice(reflect.New(reflect.SliceOf(typeOfFirst.Elem())).Elem(), valOfFirst)
+	result = reflect.AppendSlice(result, reflect.ValueOf(excepts))
+	return result.Interface()
+}
+
+func Find(source interface{}, fn Predicate) (interface{}, error) {
 	aType := reflect.TypeOf(source)
-	if aType.Kind() != reflect.Slice && aType.Kind() != reflect.Array {
-		panic("collections.Find: source must be a slice or array")
+	if aType.Kind() != reflect.Slice {
+		panic("collections.Find: source must be a slice")
 	}
 
 	sourceVal := reflect.ValueOf(source)
 	if sourceVal.Len() == 0 {
-		return nil, ErrElementNotFound
+		return nil, errors.New(ErrElementNotFound)
 	}
 
 	for i := 0; i < sourceVal.Len(); i++ {
 		s := sourceVal.Index(i).Interface()
-		if predicate(s) {
+		if fn(s) {
 			return s, nil
 		}
 	}
-	return nil, ErrElementNotFound
+	return nil, errors.New(ErrElementNotFound)
 }
 
-// 在数组中查找符合条件的元素, 如果没有找到, 则返回默认值
-// data: 数组
-// predicate: 条件
-// defaultValue: 默认值
-// return: 符合条件的元素
-func FindOrDefault(source interface{}, predicate func(a interface{}) bool, defaultValue interface{}) interface{} {
-	item, err := Find(source, predicate)
+func FindOrDefault(source interface{}, fn Predicate, defaultValue interface{}) interface{} {
+	item, err := Find(source, fn)
 	if err != nil {
-		if err == ErrElementNotFound {
+		if err.Error() == ErrElementNotFound {
 			return defaultValue
 		}
 	}
 	return item
 }
 
-// 在数组中查找符合条件的元素, 如果没有找到, 则返回空数组
-// data: 数组
-// predicate: 条件
-// return: 符合条件的元素
-func TakeWhile(data interface{}, predicate func(a interface{}) bool) interface{} {
+func TakeWhile(data interface{}, fn Predicate) interface{} {
 	aType := reflect.TypeOf(data)
-	if aType.Kind() != reflect.Slice && aType.Kind() != reflect.Array {
-		panic("collections.TakeWhile: data must be a slice or array")
+	if aType.Kind() != reflect.Slice {
+		panic("collections.TakeWhile: data must be a slice")
 	}
 	sourceVal := reflect.ValueOf(data)
 	if sourceVal.Len() == 0 {
-		return data
+		return MakeEmptySlice(aType.Elem())
 	}
-	itemType := sourceVal.Index(0).Type()
-	result := reflect.New(reflect.ArrayOf(0, itemType)).Elem()
+	result := reflect.New(reflect.SliceOf(aType.Elem())).Elem()
 	for i := 0; i < sourceVal.Len(); i++ {
 		s := sourceVal.Index(i).Interface()
-		if predicate(s) {
+		if fn(s) {
 			result = reflect.Append(result, reflect.ValueOf(s))
 		}
 	}
-	return result
+	return result.Interface()
 }
 
-// 在数组中排除符合条件的元素, 如果没有找到, 则返回空数组
-// data: 数组
-// predicate: 条件
-// return: 符合条件的元素
-func ExceptWhile(data interface{}, predicate func(a interface{}) bool) interface{} {
+func ExceptWhile(data interface{}, fn Predicate) interface{} {
 	aType := reflect.TypeOf(data)
-	if aType.Kind() != reflect.Slice && aType.Kind() != reflect.Array {
-		panic("collections.ExceptWhile: data must be a slice or array")
+	if aType.Kind() != reflect.Slice {
+		panic("collections.ExceptWhile: data must be a slice")
 	}
 	sourceVal := reflect.ValueOf(data)
 	if sourceVal.Len() == 0 {
-		return data
+		return MakeEmptySlice(aType.Elem())
 	}
-	itemType := sourceVal.Index(0).Type()
-	result := reflect.New(reflect.ArrayOf(0, itemType)).Elem()
+	result := reflect.New(reflect.SliceOf(aType.Elem())).Elem()
 	for i := 0; i < sourceVal.Len(); i++ {
 		s := sourceVal.Index(i).Interface()
-		if !predicate(s) {
+		if !fn(s) {
 			result = reflect.Append(result, reflect.ValueOf(s))
 		}
 	}
-	return result
+	return result.Interface()
 }
