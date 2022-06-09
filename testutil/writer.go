43016daac00d78f5b433cee8a6746331b71a3841
Inhere <in.798@qq.com>
2022-06-09 10:13:14 +0800

    feat: add TestWriter util struct to testutil package

---
+++ testutil/writer.go
@@ -0,0 +1,48 @@
+package testutil
+
+import (
+	"bytes"
+
+	"github.com/gookit/goutil/errorx"
+)
+
+// TestWriter struct, useful for testing
+type TestWriter struct {
+	bytes.Buffer
+	// ErrOnWrite return error on write, useful for testing
+	ErrOnWrite bool
+	// ErrOnFlush return error on flush, useful for testing
+	ErrOnFlush bool
+	// ErrOnClose return error on close, useful for testing
+	ErrOnClose bool
+}
+
+// NewTestWriter instance
+func NewTestWriter() *TestWriter {
+	return &TestWriter{}
+}
+
+// Flush implements
+func (w *TestWriter) Flush() error {
+	if w.ErrOnFlush {
+		return errorx.Raw("flush error")
+	}
+	return nil
+}
+
+// Close implements
+func (w *TestWriter) Close() error {
+	if w.ErrOnClose {
+		return errorx.Raw("close error")
+	}
+	return nil
+}
+
+// Write implements
+func (w *TestWriter) Write(p []byte) (n int, err error) {
+	if w.ErrOnWrite {
+		return 0, errorx.Raw("write error")
+	}
+
+	return w.Buffer.Write(p)
+}
