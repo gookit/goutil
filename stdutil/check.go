557aa479a42e5303fdd3240b04cd8c6a010494e4
Inhere <in.798@qq.com>
2022-07-03 17:25:40 +0800

    up: stdutil - update some std util func logic

--- stdutil/check.go
+++ stdutil/check.go
@@ -1,57 +1,48 @@
 package stdutil
 
 import (
-	"fmt"
 	"reflect"
-	"strconv"
+
+	"github.com/gookit/goutil/reflects"
 )
 
-// IsEquals(s, d interface{}) bool
+// TODO IsEqual(s, d interface{}) bool
 // IsContains(v, sub interface{}) bool
 
-// ValueIsEmpty check
-func ValueIsEmpty(v reflect.Value) bool {
-	switch v.Kind() {
-	case reflect.Invalid:
+// IsNil value check
+func IsNil(v interface{}) bool {
+	if v == nil {
 		return true
-	case reflect.String, reflect.Array:
-		return v.Len() == 0
-	case reflect.Map, reflect.Slice:
-		return v.Len() == 0 || v.IsNil()
-	case reflect.Bool:
-		return !v.Bool()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return v.Int() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return v.Uint() == 0
-	case reflect.Float32, reflect.Float64:
-		return v.Float() == 0
-	case reflect.Interface, reflect.Ptr:
-		return v.IsNil()
 	}
-
-	return reflect.DeepEqual(v.Interface(), reflect.Zero(v.Type()).Interface())
+	return reflects.IsNil(reflect.ValueOf(v))
 }
 
-// ValueLen get value length
-func ValueLen(v reflect.Value) int {
-	v = reflect.Indirect(v)
-	k := v.Kind()
-
-	// (u)int use width.
-	switch k {
-	case reflect.String:
-		return len([]rune(v.String()))
-	case reflect.Map, reflect.Array, reflect.Chan, reflect.Slice:
-		return v.Len()
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return len(strconv.FormatInt(int64(v.Uint()), 10))
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return len(strconv.FormatInt(v.Int(), 10))
-	case reflect.Float32, reflect.Float64:
-		return len(fmt.Sprint(v.Interface()))
+// IsEmpty value check
+func IsEmpty(v interface{}) bool {
+	if v == nil {
+		return true
 	}
+	return reflects.IsEmpty(reflect.ValueOf(v))
+}
 
-	// cannot get length
-	return -1
+// IsFunc value
+func IsFunc(val interface{}) bool {
+	if val == nil {
+		return false
+	}
+	return reflect.TypeOf(val).Kind() == reflect.Func
+}
+
+// ValueIsEmpty reflect value check.
+//
+// Deprecated: please use reflects.IsEmpty()
+func ValueIsEmpty(v reflect.Value) bool {
+	return reflects.IsEmpty(v)
+}
+
+// ValueLen get reflect value length
+//
+// Deprecated: please use reflects.Len()
+func ValueLen(v reflect.Value) int {
+	return reflects.Len(v)
 }
