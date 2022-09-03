4391dcf388c4a79fd4b4451688c64167096c23f7
Inhere <in.798@qq.com>
2022-09-04 00:54:27 +0800

    feat: structs - add new func SetValues for set struct field values

--- structs/README.md
+++ structs/README.md
@@ -4,13 +4,14 @@ Provide some extends util functions for struct. eg: convert, tag parse, struct d
 
 - `structs.Aliases` - implemented a simple string alias map.
 - Convert a struct to `map[string]any` data
-- Quickly init struct default value by field "default" tag.
+- Quickly init struct default values by field "default" tag.
+- Quickly set struct field values by map data
 - Parse a struct and collect tags, and parse tag value
 - And more util functions ...
 
 ## Install
 
-```bash
+```shell
 go get github.com/gookit/goutil/structs
 ```
 
@@ -52,26 +53,26 @@ map[string]interface {} { #len=2
 },
 ```
 
-### Init defaults
+### Init default values
 
 `structs.InitDefaults` Quickly init struct default value by field "default" tag.
 
 **Examples**:
 
 ```go
-	type Extra struct {
-		City   string `default:"chengdu"`
-		Github string `default:"https://github.com/inhere"`
-	}
-	type User struct {
-		Name  string `default:"inhere"`
-		Age   int    `default:"30"`
-		Extra Extra
-	}
+type Extra struct {
+    City   string `default:"chengdu"`
+    Github string `default:"https://github.com/inhere"`
+}
+type User struct {
+    Name  string `default:"inhere"`
+    Age   int    `default:"30"`
+    Extra Extra
+}
 
-	u := &User{}
-	_ = structs.InitDefaults(u, &structs.InitOptions{})
-	dump.P(u)
+u := &User{}
+_ = structs.InitDefaults(u, &structs.InitOptions{})
+dump.P(u)
 ```
 
 **Output**:
@@ -87,6 +88,42 @@ map[string]interface {} { #len=2
 },
 ```
 
+### Set values from map
+
+```go
+data := map[string]interface{}{
+    "name": "inhere",
+    "age":  234,
+    "tags": []string{"php", "go"},
+    "city": "chengdu",
+}
+
+type User struct {
+    Name string   `json:"name"`
+    Age  int      `json:"age"`
+    Tags []string `json:"tags"`
+    City string   `json:"city"`
+}
+
+u := &User{}
+err := structs.SetValues(u, data)
+dump.P(u)
+```
+
+**Output**:
+
+```shell
+&structs_test.User {
+  Name: string("inhere"), #len=6
+  Age: int(234),
+  Tags: []string [ #len=2
+    string("php"), #len=3
+    string("go"), #len=2
+  ],
+  City: string("chengdu"), #len=7
+},
+```
+
 ### Tags collect and parse
 
 Parse a struct for collect tags, and parse tag value
@@ -94,42 +131,48 @@ Parse a struct for collect tags, and parse tag value
 **Examples**:
 
 ```go
-	// eg: "desc;required;default;shorts"
-	type MyCmd struct {
-		Name string `flag:"set your name;false;INHERE;n"`
-	}
+// eg: "desc;required;default;shorts"
+type MyCmd struct {
+    Name string `flag:"set your name;false;INHERE;n"`
+}
 
-	c := &MyCmd{}
-	p := structs.NewTagParser("flag")
+c := &MyCmd{}
+p := structs.NewTagParser("flag")
 
-	sepStr := ";"
-	defines := []string{"desc", "required", "default", "shorts"}
-	p.ValueFunc = structs.ParseTagValueDefine(sepStr, defines)
+sepStr := ";"
+defines := []string{"desc", "required", "default", "shorts"}
+p.ValueFunc = structs.ParseTagValueDefine(sepStr, defines)
 
-	goutil.MustOK(p.Parse(c))
-	// dump.P(p.Tags())
-	/*
-		map[string]maputil.SMap { #len=1
-		  "Name": maputil.SMap { #len=1
-		    "flag": string("set your name;false;INHERE;n"), #len=28
-		  },
-		},
-	*/
-	fmt.Println("tags:", p.Tags())
+goutil.MustOK(p.Parse(c))
+dump.P(p.Tags())
+```
 
-	info, _ := p.Info("Name", "flag")
-	dump.P(info)
-	/*
-		maputil.SMap { #len=4
-		  "desc": string("set your name"), #len=13
-		  "required": string("false"), #len=5
-		  "default": string("INHERE"), #len=6
-		  "shorts": string("n"), #len=1
-		},
-	*/
+Output:
 
-	// Output:
-	// tags: map[Name:{flag:set your name;false;INHERE;n}]
+```shell
+map[string]maputil.SMap { #len=1
+  "Name": maputil.SMap { #len=1
+    "flag": string("set your name;false;INHERE;n"), #len=28
+  },
+},
+```
+
+**Parse tag value**
+
+```go
+info, _ := p.Info("Name", "flag")
+dump.P(info)
+```
+
+Output:
+
+```shell
+maputil.SMap { #len=4
+  "desc": string("set your name"), #len=13
+  "required": string("false"), #len=5
+  "default": string("INHERE"), #len=6
+  "shorts": string("n"), #len=1
+},
 ```
 
 ## Testings
---
+++ structs/setval.go
@@ -0,0 +1,224 @@
+package structs
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+
+	"github.com/gookit/goutil/internal/comfunc"
+	"github.com/gookit/goutil/reflects"
+)
+
+const defaultInitTag = "default"
+
+// InitOptFunc define
+type InitOptFunc func(opt *InitOptions)
+
+// InitOptions struct
+type InitOptions struct {
+	// TagName default value tag name. tag: default
+	TagName string
+	// ParseEnv var name on default value. eg: `default:"${APP_ENV}"`
+	//
+	// default: false
+	ParseEnv bool
+	// ValueHook before set value hook TODO
+	ValueHook func(val string) interface{}
+}
+
+// InitDefaults init struct default value by field "default" tag.
+//
+// TIPS:
+//
+//	Only support init set: string, bool, intX, uintX, floatX
+//
+// Example:
+//
+//	type User1 struct {
+//		Name string `default:"inhere"`
+//		Age  int32  `default:"30"`
+//	}
+//
+//	u1 := &User1{}
+//	err = structs.InitDefaults(u1)
+//	fmt.Printf("%+v\n", u1)
+//	// Output: {Name:inhere Age:30}
+func InitDefaults(ptr interface{}, optFns ...InitOptFunc) error {
+	rv := reflect.ValueOf(ptr)
+	if rv.Kind() != reflect.Ptr {
+		return errors.New("must be provider an pointer value")
+	}
+
+	rv = rv.Elem()
+	if rv.Kind() != reflect.Struct {
+		return errors.New("must be provider an struct value")
+	}
+
+	opt := &InitOptions{TagName: defaultInitTag}
+	for _, fn := range optFns {
+		fn(opt)
+	}
+
+	return initDefaults(rv, opt)
+}
+
+func initDefaults(rv reflect.Value, opt *InitOptions) error {
+	rt := rv.Type()
+
+	for i := 0; i < rt.NumField(); i++ {
+		ft := rt.Field(i)
+		// skip don't exported field
+		if ft.Name[0] >= 'a' && ft.Name[0] <= 'z' {
+			continue
+		}
+
+		fv := rv.Field(i)
+		if fv.Kind() == reflect.Struct {
+			if err := initDefaults(fv, opt); err != nil {
+				return err
+			}
+			continue
+		}
+
+		// skip on field has value
+		if !fv.IsZero() {
+			continue
+		}
+
+		tagVal := ft.Tag.Get(opt.TagName)
+		if err := initDefaultValue(fv, tagVal, opt.ParseEnv); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func initDefaultValue(fv reflect.Value, val string, parseEnv bool) error {
+	if val == "" || !fv.CanSet() {
+		return nil
+	}
+
+	// parse env var
+	if parseEnv {
+		val = comfunc.ParseEnvVar(val, nil)
+	}
+
+	// set value
+	return reflects.SetValue(fv, val)
+}
+
+/*************************************************************
+ * load values to a struct
+ *************************************************************/
+
+// SetOptFunc define
+type SetOptFunc func(opt *SetOptions)
+
+// SetOptions for set values to struct
+type SetOptions struct {
+	// FieldTagName get field name for read value. default tag: json
+	FieldTagName string
+	// ValueHook before set value hook TODO
+	ValueHook func(val interface{}) interface{}
+
+	// ParseDefault init default value by DefaultValTag tag value.
+	// default: false
+	//
+	// see InitDefaults()
+	ParseDefault bool
+	// DefaultValTag name. tag: default
+	DefaultValTag string
+	// ParseDefaultEnv parse env var on default tag. eg: `default:"${APP_ENV}"`
+	//
+	// default: false
+	ParseDefaultEnv bool
+}
+
+// SetValues set data values to struct ptr
+//
+// TIPS:
+//
+//	Only support set: string, bool, intX, uintX, floatX
+func SetValues(ptr interface{}, data map[string]interface{}, optFns ...SetOptFunc) error {
+	rv := reflect.ValueOf(ptr)
+	if rv.Kind() != reflect.Ptr {
+		return errors.New("must be provider an pointer value")
+	}
+
+	rv = rv.Elem()
+	if rv.Kind() != reflect.Struct {
+		return errors.New("must be provider an struct value")
+	}
+
+	opt := &SetOptions{
+		FieldTagName:  "json",
+		DefaultValTag: defaultInitTag,
+	}
+
+	for _, fn := range optFns {
+		fn(opt)
+	}
+	return setValues(rv, data, opt)
+}
+
+func setValues(rv reflect.Value, data map[string]interface{}, opt *SetOptions) error {
+	if data == nil || len(data) == 0 {
+		return nil
+	}
+
+	rt := rv.Type()
+
+	for i := 0; i < rt.NumField(); i++ {
+		ft := rt.Field(i)
+		name := ft.Name
+		// skip don't exported field
+		if name[0] >= 'a' && name[0] <= 'z' {
+			continue
+		}
+
+		// get field name
+		tagVal, ok := ft.Tag.Lookup(opt.FieldTagName)
+		if ok {
+			info, err := ParseTagValueDefault(name, tagVal)
+			if err != nil {
+				return err
+			}
+
+			name = info.Get("name")
+		}
+
+		fv := rv.Field(i)
+		val, ok := data[name]
+
+		// set field value by default tag.
+		if !ok && fv.IsZero() {
+			defVal := ft.Tag.Get(opt.DefaultValTag)
+
+			if err := initDefaultValue(fv, defVal, opt.ParseDefaultEnv); err != nil {
+				return err
+			}
+			continue
+		}
+
+		// field is struct
+		if fv.Kind() == reflect.Struct {
+			asMp, ok := val.(map[string]interface{})
+			if !ok {
+				return fmt.Errorf("field is struct, must provide map data value")
+			}
+
+			if err := setValues(fv, asMp, opt); err != nil {
+				return err
+			}
+			continue
+		}
+
+		// set field value
+		if err := reflects.SetValue(fv, val); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
