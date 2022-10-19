0aaeb53dab795cb92ead3514f4eab6629ccf4f47
Inhere <in.798@qq.com>
2022-10-19 22:35:17 +0800

    up: std - remove some unused files and pkg on std

--- stdutil/chan.go
+++ stdutil/chan.go
@@ -1,6 +1,8 @@
 package stdutil
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"os"
 	"os/signal"
@@ -25,4 +27,37 @@ func Go(f func() error) error {
 	}()
 
 	return <-ch
+}
+
+// SignalHandler returns an actor, i.e. an execute and interrupt func, that
+// terminates with SignalError when the process receives one of the provided
+// signals, or the parent context is canceled.
+//
+// from https://github.com/oklog/run/blob/master/actors.go
+func SignalHandler(ctx context.Context, signals ...os.Signal) (execute func() error, interrupt func(error)) {
+	ctx, cancel := context.WithCancel(ctx)
+	return func() error {
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, signals...)
+			defer signal.Stop(c)
+			select {
+			case sig := <-c:
+				return SignalError{Signal: sig}
+			case <-ctx.Done():
+				return ctx.Err()
+			}
+		}, func(error) {
+			cancel()
+		}
+}
+
+// SignalError is returned by the signal handler's execute function
+// when it terminates due to a received signal.
+type SignalError struct {
+	Signal os.Signal
+}
+
+// Error implements the error interface.
+func (e SignalError) Error() string {
+	return fmt.Sprintf("received signal %s", e.Signal)
 }
