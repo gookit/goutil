d0dade8703da172f80424ff8c85ec759ec8273ae
Inhere <in.798@qq.com>
2023-01-21 13:41:11 +0800

    :sparkles: feat: testutil - add new util func IsMatchAll()
    - for match input text by keywords

--- strutil/textutil/textutil.go
+++ strutil/textutil/textutil.go
@@ -1,9 +1,36 @@
 // Package textutil provide some extra text handle util
 package textutil
 
+import "strings"
+
 // ReplaceVars by regex replace given tpl vars.
 //
 // If format is empty, will use {const defaultVarFormat}
 func ReplaceVars(text string, vars map[string]any, format string) string {
 	return NewVarReplacer(format).Replace(text, vars)
+}
+
+// IsMatchAll keywords in the give text string.
+//
+// TIP: can use ^ for exclude match.
+func IsMatchAll(s string, keywords []string) bool {
+	for _, keyword := range keywords {
+		if keyword == "" {
+			continue
+		}
+
+		// exclude
+		if keyword[0] == '^' && len(keyword) > 1 {
+			if strings.Contains(s, keyword[1:]) {
+				return false
+			}
+			continue
+		}
+
+		// include
+		if !strings.Contains(s, keyword) {
+			return false
+		}
+	}
+	return true
 }
