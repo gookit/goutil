06b949dce2582696bfb843434cfcd1c15a60fd59
Inhere <in.798@qq.com>
2023-02-13 19:16:17 +0800

    :recycle: up(errorx): add errorx/panics from testutil/panics

--- errorx/panics/assert_test.go
+++ errorx/panics/assert_test.go
@@ -3,8 +3,8 @@ package panics_test
 import (
 	"testing"
 
+	"github.com/gookit/goutil/errorx/panics"
 	"github.com/gookit/goutil/testutil/assert"
-	"github.com/gookit/goutil/testutil/panics"
 )
 
 func TestIsTrue(t *testing.T) {
