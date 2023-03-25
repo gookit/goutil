59575e67dd4927c403e6283981d250edcd3925b7
Inhere <in.798@qq.com>
2023-03-25 20:03:34 +0800

    :necktie: up(fs): update the Realpath logic, will always return abs path

--- fsutil/info_nonwin.go
+++ fsutil/info_nonwin.go
@@ -4,9 +4,17 @@ package fsutil
 
 import (
 	"path"
+
+	"github.com/gookit/goutil/internal/comfunc"
+	"github.com/gookit/goutil/sysutil"
 )
 
 // Realpath returns the shortest path name equivalent to path by purely lexical processing.
 func Realpath(pathStr string) string {
+	pathStr = comfunc.ExpandHome(pathStr)
+
+	if !IsAbsPath(pathStr) {
+		pathStr = JoinSubPaths(sysutil.Workdir(), pathStr)
+	}
 	return path.Clean(pathStr)
 }
