a7457e98e3d5735db10c374bd4368c4ab37905a4
Inhere <in.798@qq.com>
2023-06-19 09:24:35 +0800

    :sparkles: feat: testutil - add new func NewEchoServer() for start an echo server.
    - useful for write http request tests

--- testutil/httpmock.go
+++ testutil/httpmock.go
@@ -1,6 +1,7 @@
 package testutil
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -101,4 +102,183 @@ func MockRequest(h http.Handler, method, path string, data *MD) *httptest.Respon
 	// s := httptest.NewServer()
 	h.ServeHTTP(w, r)
 	return w
+}
+
+// EchoReply http response data reply model
+type EchoReply struct {
+	Origin string `json:"origin"`
+	Url    string `json:"url"`
+	Method string `json:"method"`
+	// Query data
+	Query   map[string]any `json:"query,omitempty"`
+	Headers map[string]any `json:"headers,omitempty"`
+	Form    map[string]any `json:"form,omitempty"`
+	// Body data string
+	Body  string         `json:"body,omitempty"`
+	Json  any            `json:"json,omitempty"`
+	Files map[string]any `json:"files,omitempty"`
+}
+
+// ContentType get content type
+func (r *EchoReply) ContentType() string {
+	return r.Headers["Content-Type"].(string)
+}
+
+// NewEchoServer create an echo server for testing.
+//
+// Usage on testing:
+//
+//	var testSrvAddr string
+//
+//	func TestMain(m *testing.M) {
+//		// create server
+//		s := testutil.NewEchoServer()
+//		defer s.Close()
+//		testSrvAddr = "http://" + s.Listener.Addr().String()
+//		fmt.Println("Test server listen on:", testSrvAddr)
+//
+//		m.Run()
+//	}
+//
+//	// in a test case ...
+//	res := http.Get(testSrvAddr)
+//	rpl := testutil.ParseRespToReply(res)
+//	// assert ...
+func NewEchoServer() *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.Header().Set("Server", "goutil/echo-server")
+		w.WriteHeader(http.StatusOK)
+		// w.Header().Set("Connection", "close")
+
+		enc := json.NewEncoder(w)
+		enc.SetIndent("", "  ")
+		err := enc.Encode(BuildEchoReply(r))
+		if err != nil {
+			_, _ = w.Write([]byte(`{"error": "encode error"}`))
+		}
+	}))
+}
+
+// BuildEchoReply build reply body data
+func BuildEchoReply(r *http.Request) *EchoReply {
+	// get headers
+	headers := stringsMapToAnyMap(r.Header)
+
+	// get query args
+	args := stringsMapToAnyMap(r.URL.Query())
+	cType := r.Header.Get("Content-Type")
+	method := strings.ToUpper(r.Method)
+
+	var jsonBody any
+	var bodyStr string
+	var form, files map[string]any
+
+	if method == "POST" || method == "PUT" || method == "PATCH" {
+		// get form data
+		_ = r.ParseForm()
+		form = stringsMapToAnyMap(r.Form)
+
+		// get form files
+		if r.MultipartForm != nil {
+			files = make(map[string]any)
+			for k, fhs := range r.MultipartForm.File {
+				if len(fhs) == 1 {
+					files[k] = fhs[0]
+				} else {
+					files[k] = fhs
+				}
+			}
+		}
+
+		// get body data
+		if r.Body != nil {
+			// defer r.Body.Close()
+			body, _ := io.ReadAll(r.Body)
+			bodyStr = string(body)
+
+			// try to parse json
+			if len(body) > 0 && strings.HasPrefix(cType, "application/json") {
+				_ = json.Unmarshal(body, &jsonBody)
+			}
+		}
+	}
+
+	return &EchoReply{
+		Url:     r.URL.String(),
+		Origin:  r.RemoteAddr,
+		Method:  method,
+		Body:    bodyStr,
+		Json:    jsonBody,
+		Query:   args,
+		Form:    form,
+		Files:   files,
+		Headers: headers,
+	}
+}
+
+/*
+// HTTP tool for testing
+var HTTP = &HTTPTool{}
+
+// HTTPTool http tool for testing
+type HTTPTool struct {
+}
+
+func (ht *HTTPTool) ParseRespToReply(r *http.Response) *EchoReply {
+	return ParseRespToReply(r)
+}
+
+func (ht *HTTPTool) ParseBodyToReply(bd io.ReadCloser) *EchoReply {
+	return ParseBodyToReply(bd)
+}
+*/
+
+// ParseRespToReply parse http response to reply
+func ParseRespToReply(w *http.Response) *EchoReply {
+	if w.Body == nil {
+		return nil
+	}
+
+	if w.Request != nil && w.Request.Method == "HEAD" {
+		req := w.Request
+		rpl := &EchoReply{
+			Url:     req.URL.String(),
+			Method:  req.Method,
+			Headers: stringsMapToAnyMap(req.Header),
+		}
+		return rpl
+	}
+
+	return ParseBodyToReply(w.Body)
+}
+
+// ParseBodyToReply parse http body to reply
+func ParseBodyToReply(bd io.ReadCloser) *EchoReply {
+	rpl := &EchoReply{}
+	if bd == nil {
+		return rpl
+	}
+
+	err := json.NewDecoder(bd).Decode(rpl)
+	if err != nil {
+		panic(err)
+	}
+	return rpl
+}
+
+func stringsMapToAnyMap(ssMp map[string][]string) map[string]any {
+	if len(ssMp) == 0 {
+		return nil
+	}
+
+	anyMp := make(map[string]any, len(ssMp))
+	for k, v := range ssMp {
+		if len(v) == 1 {
+			anyMp[k] = v[0]
+			continue
+		}
+		anyMp[k] = v
+	}
+	return anyMp
 }
--- testutil/httpmock_test.go
+++ testutil/httpmock_test.go
@@ -1,11 +1,13 @@
 package testutil_test
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"strings"
 	"testing"
 
+	"github.com/gookit/goutil/dump"
 	"github.com/gookit/goutil/testutil"
 	"github.com/gookit/goutil/testutil/assert"
 )
@@ -29,4 +31,36 @@ func TestMockRequest(t *testing.T) {
 
 	w = testutil.MockRequest(r, "POST", "/", &testutil.MD{Body: strings.NewReader("BODY")})
 	assert.Eq(t, "hello!BODY", w.Body.String())
+}
+
+var testSrvAddr string
+
+func TestMain(m *testing.M) {
+	s := testutil.NewEchoServer()
+	defer s.Close()
+
+	testSrvAddr = "http://" + s.Listener.Addr().String()
+	fmt.Println("server addr:", testSrvAddr)
+
+	m.Run()
+}
+
+func TestNewEchoServer(t *testing.T) {
+	r, err := http.Post(testSrvAddr, "text/plain", strings.NewReader("hello!"))
+	assert.NoErr(t, err)
+
+	rpl := testutil.ParseRespToReply(r)
+	dump.P(rpl)
+	assert.Eq(t, "POST", rpl.Method)
+	assert.Eq(t, "text/plain", rpl.ContentType())
+	assert.Eq(t, "hello!", rpl.Body)
+
+	r, err = http.Post(testSrvAddr, "application/json", strings.NewReader(`{"name": "inhere", "age": 18}`))
+	assert.NoErr(t, err)
+
+	rpl = testutil.ParseRespToReply(r)
+	dump.P(rpl)
+	assert.Eq(t, "POST", rpl.Method)
+	assert.Eq(t, "application/json", rpl.ContentType())
+	assert.Eq(t, `{"name": "inhere", "age": 18}`, rpl.Body)
 }
