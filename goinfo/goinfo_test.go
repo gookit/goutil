dd238bb68f596e6d10cfe63d86f900604be321fe
Inhere <in.798@qq.com>
2023-07-19 15:12:32 +0800

    :necktie: update: goinfo - move parse go info func to package goinfo from sysutil

--- goinfo/goinfo_test.go
+++ goinfo/goinfo_test.go
@@ -3,10 +3,27 @@ package goinfo_test
 import (
 	"testing"
 
+	"github.com/gookit/goutil/dump"
 	"github.com/gookit/goutil/goinfo"
 	"github.com/gookit/goutil/testutil/assert"
 )
 
 func TestGoVersion(t *testing.T) {
 	assert.NotEmpty(t, goinfo.GoVersion())
+}
+
+func TestOsGoInfo(t *testing.T) {
+	assert.NotEmpty(t, goinfo.GoVersion())
+
+	info, err := goinfo.ParseGoVersion("go version go1.19.2 darwin/amd64")
+	assert.NoErr(t, err)
+	assert.NotEmpty(t, info)
+	assert.Eq(t, "1.19.2", info.Version)
+	assert.Eq(t, "darwin", info.GoOS)
+	assert.Eq(t, "amd64", info.Arch)
+
+	info, err = goinfo.OsGoInfo()
+	assert.NoErr(t, err)
+	assert.NotEmpty(t, info)
+	dump.P(info)
 }
--- sysutil/sysgo.go
+++ sysutil/sysgo.go
@@ -1,10 +1,9 @@
 package sysutil
 
 import (
-	"errors"
-	"regexp"
 	"runtime"
-	"strings"
+
+	"github.com/gookit/goutil/goinfo"
 )
 
 // GoVersion get go runtime version. eg: "1.18.2"
@@ -12,22 +11,10 @@ func GoVersion() string {
 	return runtime.Version()[2:]
 }
 
-// GoInfo define
-//
-// On os by:
-//
-//	go env GOVERSION GOOS GOARCH
-//	go version // "go version go1.19 darwin/amd64"
-type GoInfo struct {
-	Version string
-	GoOS    string
-	Arch    string
-}
+// GoInfo define. alias of goinfo.GoInfo
+type GoInfo = goinfo.GoInfo
 
-// match "go version go1.19 darwin/amd64"
-var goVerRegex = regexp.MustCompile(`\sgo([\d.]+)\s(\w+)/(\w+)`)
-
-// ParseGoVersion get info by parse `go version` results.
+// ParseGoVersion get info by parse `go version` results. alias of goinfo.ParseGoVersion()
 //
 // Examples:
 //
@@ -39,38 +26,10 @@ var goVerRegex = regexp.MustCompile(`\sgo([\d.]+)\s(\w+)/(\w+)`)
 //		info, err := sysutil.ParseGoVersion()
 //	 	dump.P(info)
 func ParseGoVersion(line string) (*GoInfo, error) {
-	// eg: [" go1.19 darwin/amd64", "1.19", "darwin", "amd64"]
-	lines := goVerRegex.FindStringSubmatch(line)
-	if len(lines) != 4 {
-		return nil, errors.New("returns go info is not full")
-	}
-
-	info := &GoInfo{}
-	info.Version = strings.TrimPrefix(lines[1], "go")
-	info.GoOS = lines[2]
-	info.Arch = lines[3]
-
-	return info, nil
+	return goinfo.ParseGoVersion(line)
 }
 
-// OsGoInfo fetch and parse
+// OsGoInfo fetch and parse. alias of goinfo.OsGoInfo()
 func OsGoInfo() (*GoInfo, error) {
-	cmdArgs := []string{"env", "GOVERSION", "GOOS", "GOARCH"}
-	line, err := ExecCmd("go", cmdArgs)
-	if err != nil {
-		return nil, err
-	}
-
-	lines := strings.Split(strings.TrimSpace(line), "\n")
-
-	if len(lines) != len(cmdArgs)-1 {
-		return nil, errors.New("returns go info is not full")
-	}
-
-	info := &GoInfo{}
-	info.Version = strings.TrimPrefix(lines[0], "go")
-	info.GoOS = lines[1]
-	info.Arch = lines[2]
-
-	return info, nil
+	return goinfo.OsGoInfo()
 }
