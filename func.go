6e8aafc1e2a87d5b47b7574827dda1190cc89a20
wzs <zhensheng.five@gmail.com>
2023-09-04 19:30:41 +0800

    [feature] add func.saferun, automatically recover error when func panic

--- func.go
+++ func.go
@@ -1,5 +1,7 @@
 package goutil
 
+import "fmt"
+
 // Go is a basic promise implementation: it wraps calls a function in a goroutine
 // and returns a channel which will later return the function's return value.
 func Go(f func() error) error {
@@ -27,4 +29,54 @@ func CallOrElse(cond bool, okFn, elseFn ErrFunc) error {
 		return okFn()
 	}
 	return elseFn()
+}
+
+// SafeRun sync run a func. If the func panics, the panic value is returned as an error.
+func SafeRun(fn func()) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			if e, ok := r.(error); ok {
+				err = e
+			} else {
+				err = fmt.Errorf("%v", r)
+			}
+		}
+	}()
+	fn()
+	return nil
+}
+
+// SafeRun sync run a func with error.
+// If the func panics, the panic value is returned as an error.
+func SafeRunWithError(fn func() error) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			if e, ok := r.(error); ok {
+				err = e
+			} else {
+				err = fmt.Errorf("%v", r)
+			}
+		}
+	}()
+	return fn()
+}
+
+// SafeGo async run a func.
+// If the func panics, the panic value will be handle by errHandler.
+func SafeGo(fn func(), errHandler func(error)) {
+	go func() {
+		if err := SafeRun(fn); err != nil {
+			errHandler(err)
+		}
+	}()
+}
+
+// SafeGoWithError async run a func with error.
+// If the func panics, the panic value will be handle by errHandler.
+func SafeGoWithError(fn func() error, errHandler func(error)) {
+	go func() {
+		if err := SafeRunWithError(fn); err != nil {
+			errHandler(err)
+		}
+	}()
 }
